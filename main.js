7f2020e39dede6fe04be57bfbbf41271c0339d51
unknown <denizchildir88@gmail.com>
2023-09-22 01:12:04 -0700

    7

--- main.js
+++ main.js
@@ -1,68 +1,84 @@
 const video = document.getElementById('video');
-const startRecordingButton = document.getElementById('startRecording');
-const stopRecordingButton = document.getElementById('stopRecording');
 let mediaRecorder;
 let recordedChunks = [];
 let recordInterval;
+let db;
+
+// Initialize IndexedDB
+function initDB() {
+  const request = indexedDB.open('videosDB', 1);
+  request.onerror = function (event) {
+    console.error('Error opening IndexedDB', event);
+  };
+  request.onupgradeneeded = function (event) {
+    db = event.target.result;
+    db.createObjectStore('videos', { autoIncrement: true });
+  };
+  request.onsuccess = function (event) {
+    db = event.target.result;
+  };
+}
 
 async function initCamera() {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
-        video.srcObject = stream;
-        mediaRecorder = new MediaRecorder(stream);
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+    video.srcObject = stream;
+    mediaRecorder = new MediaRecorder(stream);
 
-        mediaRecorder.ondataavailable = (event) => {
-            if (event.data.size > 0) {
-                recordedChunks.push(event.data);
-            }
-        };
+    mediaRecorder.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        recordedChunks.push(event.data);
+      }
+    };
 
-        mediaRecorder.onstop = () => {
-            const blob = new Blob(recordedChunks, { type: 'video/webm' });
-            recordedChunks = [];
-            
-            // Save Blob Locally
-            const url = URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.style.display = 'none';
-            a.href = url;
-            a.download = 'recorded-video.webm';
-            document.body.appendChild(a);
-            a.click();
-            window.URL.revokeObjectURL(url);
-            
-            // Upload to Firebase
-            uploadToFirebase(blob);
+    mediaRecorder.onstop = () => {
+      const blob = new Blob(recordedChunks, { type: 'video/webm' });
+      recordedChunks = [];
 
-            if (!stopRecordingButton.disabled) {
-                startNewRecording();
-            }
-        };
+      // Store blob in IndexedDB
+      storeBlob(blob);
 
-        startRecordingButton.disabled = false;
-    } catch (error) {
-        console.error('Error accessing camera:', error);
-    }
+      // Upload to Firebase
+      uploadToFirebase(blob);
+
+      // Restart the recording automatically
+      startNewRecording();
+    };
+
+    // Start the first recording
+    startNewRecording();
+  } catch (error) {
+    console.error('Error accessing camera:', error);
+  }
 }
 
 function startNewRecording() {
-    mediaRecorder.start();
-    recordInterval = setTimeout(() => {
-        mediaRecorder.stop();
-    }, 8000); // stop recording after 15 seconds
+  mediaRecorder.start();
+  recordInterval = setTimeout(() => {
+    mediaRecorder.stop();
+  }, 15000); // stop recording after 15 seconds
 }
 
-startRecordingButton.addEventListener('click', () => {
-    startNewRecording();
-    startRecordingButton.disabled = true;
-    stopRecordingButton.disabled = false;
-});
+// Store blob in IndexedDB
+function storeBlob(blob) {
+  const transaction = db.transaction(['videos'], 'readwrite');
+  const objectStore = transaction.objectStore('videos');
+  objectStore.add(blob);
+}
 
-stopRecordingButton.addEventListener('click', () => {
-    clearTimeout(recordInterval); // Clear the existing interval
-    mediaRecorder.stop(); // Stop the current recording
-    startRecordingButton.disabled = false;
-    stopRecordingButton.disabled = true;
-});
+// Dummy function for uploading to Firebase
+// Replace with actual Firebase upload logic
+function uploadToFirebase(blob) {
+  const storageRef = firebase.storage().ref();
+  const videoRef = storageRef.child(`videos/${new Date().toISOString()}.webm`);
+  
+  videoRef.put(blob).then((snapshot) => {
+    console.log('Uploaded a blob or file!', snapshot);
+  }).catch((error) => {
+    console.error('Upload failed:', error);
+  });
+}
 
+
+initDB();
 initCamera();
