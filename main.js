1f3b16b86f61aea7779c11e9cbb6d7318dfc22e2
unknown <denizchildir88@gmail.com>
2023-09-22 15:51:17 -0700

    a

--- main.js
+++ main.js
@@ -4,84 +4,89 @@ const stopRecordingButton = document.getElementById('stopRecording');
 let mediaRecorder;
 let recordedChunks = [];
 let recordInterval;
-let db;
-let isRecording = false;
+
+// Define the name of your IndexedDB and the object store
+const dbName = 'videoDB';
+const storeName = 'videos';
 
 // Initialize IndexedDB
 function initDB() {
-  const request = indexedDB.open('videosDB', 1);
-  request.onerror = function (event) {
-    console.error('Error opening IndexedDB', event);
-  };
-  request.onupgradeneeded = function (event) {
-    db = event.target.result;
-    db.createObjectStore('videos', { autoIncrement: true });
-  };
-  request.onsuccess = function (event) {
-    db = event.target.result;
-  };
+    const request = indexedDB.open(dbName);
+
+    request.onupgradeneeded = (event) => {
+        const db = event.target.result;
+        if (!db.objectStoreNames.contains(storeName)) {
+            db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
+        }
+    };
+
+    request.onerror = (event) => {
+        console.error('Could not open IndexedDB:', event.target.errorCode);
+    };
 }
 
 async function initCamera() {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
-    video.srcObject = stream;
-    mediaRecorder = new MediaRecorder(stream);
-    mediaRecorder.ondataavailable = (event) => {
-      if (event.data.size > 0) recordedChunks.push(event.data);
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+        video.srcObject = stream;
+        mediaRecorder = new MediaRecorder(stream);
+
+        mediaRecorder.ondataavailable = (event) => {
+            if (event.data.size > 0) {
+                recordedChunks.push(event.data);
+            }
+        };
+
+        mediaRecorder.onstop = () => {
+            const blob = new Blob(recordedChunks, { type: 'video/webm' });
+            recordedChunks = [];
+            storeInIDB(blob);
+            uploadToFirebase(blob);
+            if (!stopRecordingButton.disabled) {
+                startNewRecording();
+            }
+        };
+
+        startRecordingButton.disabled = false;
+    } catch (error) {
+        console.error('Error accessing camera:', error);
+    }
+}
+
+function startNewRecording() {
+    mediaRecorder.start();
+    recordInterval = setTimeout(() => {
+        mediaRecorder.stop();
+    }, 8000); // 8 seconds
+}
+
+// Function to store video blob in IndexedDB
+function storeInIDB(blob) {
+    const request = indexedDB.open(dbName);
+    request.onsuccess = (event) => {
+        const db = event.target.result;
+        const transaction = db.transaction(storeName, 'readwrite');
+        const store = transaction.objectStore(storeName);
+        store.add({ video: blob, timestamp: new Date() });
     };
-    mediaRecorder.onstop = stopHandler;
-  } catch (error) {
-    console.error('Error accessing the camera:', error);
-  }
+
+    request.onerror = (event) => {
+        console.error('Could not open IndexedDB:', event.target.errorCode);
+    };
 }
 
-function startHandler() {
-  console.log('Start button clicked');
-  if (isRecording) return;
-  isRecording = true;
-  startRecording();
-}
+startRecordingButton.addEventListener('click', () => {
+    startNewRecording();
+    startRecordingButton.disabled = true;
+    stopRecordingButton.disabled = false;
+});
 
-function stopHandler() {
-  console.log('Stop button clicked');
-  if (!isRecording) return;
-  isRecording = false;
-  clearTimeout(recordInterval);
-  mediaRecorder.stop();
-  processRecording();
-}
+stopRecordingButton.addEventListener('click', () => {
+    clearTimeout(recordInterval); // Clear the existing interval
+    mediaRecorder.stop(); // Stop the current recording
+    startRecordingButton.disabled = false;
+    stopRecordingButton.disabled = true;
+});
 
-function startRecording() {
-  if (!isRecording) return;
-  recordedChunks = [];
-  mediaRecorder.start();
-  recordInterval = setTimeout(() => {
-    mediaRecorder.stop();
-    processRecording();
-    if (isRecording) startRecording(); // If still recording, start a new segment.
-  }, 8000);
-}
-
-function processRecording() {
-  const blob = new Blob(recordedChunks, { type: 'video/webm' });
-  storeBlob(blob);
-  uploadToFirebase(blob);
-}
-
-function storeBlob(blob) {
-  const transaction = db.transaction(['videos'], 'readwrite');
-  const objectStore = transaction.objectStore('videos');
-  objectStore.add(blob);
-}
-
-function uploadToFirebase(blob) {
-  // Add the Firebase upload logic here.
-  console.log('Attempt to upload to Firebase');
-}
-
-startRecordingButton.addEventListener('click', startHandler);
-stopRecordingButton.addEventListener('click', stopHandler);
-
-initDB();
-initCamera();
+initDB(); // Initialize the IndexedDB when the script loads
+initCamera(); // Initialize the camera when the script loads
